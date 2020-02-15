6ff20c99eb27679152c5c0b7777bd468d0d418c7
JBenne98 <jgb83474@creighton.edu>
2020-02-15 13:25:14 -0600

    changes

--- forms/friends/friends.js
+++ forms/friends/friends.js
@@ -1,21 +1,20 @@
-/*
-prompt("Enter the name of your first of three friends.");
-Prompt("Enter the name of your second of three friends.");
-prompt("Enter the name of your third of three friends.");
-alert("Enter a 1 if your first person was your best friend, a 2 for the second, and a 3 for the third")
 
-var friendNames = ();
+let firstFriend=prompt("Enter the name of your first of three friends.");
+let secondFriend=prompt("Enter the name of your second of three friends.");
+let thirdFriend=prompt("Enter the name of your third of three friends.");
+let best=prompt("Enter a 1 if your first person was your best friend, a 2 for the second, and a 3 for the third")
 
-friendNames[1] = firstFriend
-friendNames[2] = secondFriend
-friendNames[3] = thirdFriend
+let friendNames = [];
+
+friendNames[0] = firstFriend
+friendNames[1] = secondFriend
+friendNames[2] = thirdFriend
 let bestFriend = friendNames[best - 1]
 
-if (bestFriend.firstChar() == "A")
-   alert("Your best friends' name starts with A.")
-else if (bestFriend.firstChar() = "B")
-   alert("Your best friends' name starts with B.")
-else 
-   alert("Your name starts with something other than A or B.")
-
-
+if (bestFriend.charAt(0) == "A"){
+   alert("Your best friends\' name starts with A.")
+   } else if (bestFriend.charAt(0) == "B"){
+   alert("Your best friends\' name starts with B.")
+}else {
+   alert("Your best friends\` name starts with something other than A or B.")
+}
